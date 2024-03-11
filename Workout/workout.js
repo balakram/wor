809ef48bf5dd42fa4f39edfac2f6178be57bb781
BALAKRAM TUDU <48530348+balakram@users.noreply.github.com>
2024-03-11 22:10:03 +0530

    Delete Workout directory

--- Workout/workout.js
+++
@@ -1,8 +0,0 @@
-// JavaScript code to toggle between dark and light themes
-const themeSwitcher = document.getElementById('theme-switcher');
-const body = document.body;
-
-themeSwitcher.addEventListener('click', function() {
-    // Toggle the class to switch between themes
-    body.classList.toggle('dark-theme');
-});
